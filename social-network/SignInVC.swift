20a44907181a4fc0a3f2885fb6f0c384e389b896
Hamid <Hamid@MacBook-Pro-3.local>
2017-08-01 16:14:20 +0430

    Completed setting up Firebase

---
+++ social-network/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  social-network
+//
+//  Created by Hamid on 5/10/1396 AP.
+//  Copyright © 1396 AP Hamid. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
