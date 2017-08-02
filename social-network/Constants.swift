89b29ee54d6954947421f671131420c42561aa79
Hamid <Hamid@MacBook-Pro-3.local>
2017-08-02 12:28:56 +0430

    Sign in ui custom classes complected

---
+++ social-network/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  social-network
+//
+//  Created by Hamid on 5/11/1396 AP.
+//  Copyright © 1396 AP Hamid. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ social-network/FancyView.swift
@@ -0,0 +1,24 @@
+//
+//  FancyView.swift
+//  social-network
+//
+//  Created by Hamid on 5/11/1396 AP.
+//  Copyright © 1396 AP Hamid. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 4.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+
+}
